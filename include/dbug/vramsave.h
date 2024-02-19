f564d06e47f11948ba4b266043491580fc6a1352
Regan Green <regan_green27@hotmail.com>
2024-02-19 02:43:50 -0500

    VramSaveBMP

--- include/dbug/vramsave.h
+++ include/dbug/vramsave.h
@@ -4,36 +4,7 @@
 #include <eetypes.h>
 #include <libgraph.h>
 
-typedef struct { // 0x10
-	/* 0x0 */ char FileId[4];
-	/* 0x4 */ u_char FormatVersion;
-	/* 0x5 */ u_char FormatId;
-	/* 0x6 */ u_short Pictures;
-	/* 0x8 */ char Reserved[8];
-} TIM2_FILEHEADER;
-
-typedef struct { // 0x30
-	/* 0x00 */ u_int TotalSize;
-	/* 0x04 */ u_int ClutSize;
-	/* 0x08 */ u_int ImageSize;
-	/* 0x0c */ u_short HeaderSize;
-	/* 0x0e */ u_short ClutColors;
-	/* 0x10 */ u_char PictFormat;
-	/* 0x11 */ u_char MipMapTextures;
-	/* 0x12 */ u_char ClutType;
-	/* 0x13 */ u_char ImageType;
-	/* 0x14 */ u_short ImageWidth;
-	/* 0x16 */ u_short ImageHeight;
-	/* 0x18 */ u_long GsTex0;
-	/* 0x20 */ u_long GsTex1;
-	/* 0x28 */ u_int GsRegs;
-	/* 0x2c */ u_int GsTexClut;
-} TIM2_PICTUREHEADER;
-
-typedef struct { // 0x40
-	/* 0x00 */ TIM2_FILEHEADER t2h;
-	/* 0x10 */ TIM2_PICTUREHEADER t2p;
-} TIM2SETINFO;
+#include "os/tim2.h"
 
 typedef struct { // 0x38
     /* 0x00 */ u_char pad[2];
--- src/dbug/vramsave.c
+++ src/dbug/vramsave.c
@@ -49,14 +49,14 @@ static u_int BekiDat(u_int size) {
     return i;
 }
 
-#if 1
+#if 0
 void VramSave(u_char *fname, int wsize, int hsize, int id) {
     int fd;
     u_char fname_tmp[64];
     u_long128 *dst1_pp;
     int i;
     
-    sprintf(fname_tmp, "s_host:%s", fname);
+    sprintf(fname_tmp, "host:%s", fname);
     printf("vram save [%s]\n", fname_tmp);
 
     fd = sceOpen(fname_tmp, SCE_TRUNC | SCE_CREAT | SCE_WRONLY);
@@ -108,7 +108,89 @@ INCLUDE_RODATA(const s32, "dbug/vramsave", D_00390F30);
 
 INCLUDE_RODATA(const s32, "dbug/vramsave", D_00390F48);
 
+#if 0
+void VramSaveBMP(u_char *fname, int wsize, int hsize, int id) {
+	int fd;
+	u_char fname_tmp[64];
+	u_char *dst1_pp;
+	u_char *tr_pp;
+	u_char *img_pp;
+	int i;
+	int j;
+	u_char *moto_pp;
+	u_char *imgtr_pp;
+    
+    sprintf(fname_tmp, "host:%s", fname);
+    printf("vram save BMP [%s]\n", fname_tmp);
+
+    fd = sceOpen(fname_tmp, SCE_TRUNC | SCE_CREAT | SCE_WRONLY);
+    if (fd < 0) {
+        printf("file open error!![%s]\n", fname_tmp);
+        return;
+    }
+
+    dst1_pp = malloc(wsize * hsize * 4);
+    tr_pp = malloc(wsize * hsize * 3 + sizeof(BMP_HEADER));
+
+    sceGsSetDefStoreImage(
+        &gs_simage,
+        (id * wsize * hsize) / 64, wsize / 64,
+        0,
+        0, 0, wsize, hsize
+    );
+    FlushCache(WRITEBACK_DCACHE);
+    if (sceGsExecStoreImage(&gs_simage, (u_long128*)dst1_pp) < 0) {
+        printf("vramsave Timeout error!!\n");
+        free(tr_pp);
+        free(dst1_pp);
+        sceClose(fd);
+        return;
+    }
+    sceGsSyncPath(0, 0);
+    
+    ((BMP_HEADER*)tr_pp)->tsize = (wsize * hsize * 3) + (sizeof(BMP_HEADER) - 2);
+    ((BMP_HEADER*)tr_pp)->id[0] = 'B';
+    ((BMP_HEADER*)tr_pp)->id[1] = 'M';
+    ((BMP_HEADER*)tr_pp)->reserved[0] = 0;
+    ((BMP_HEADER*)tr_pp)->reserved[1] = 0;
+    ((BMP_HEADER*)tr_pp)->ofsbit = 0x36;
+    ((BMP_HEADER*)tr_pp)->header = 0x28;
+    ((BMP_HEADER*)tr_pp)->imageW = wsize;
+    ((BMP_HEADER*)tr_pp)->imageH = hsize;
+    ((BMP_HEADER*)tr_pp)->planes = 1;
+    ((BMP_HEADER*)tr_pp)->bitPerPixel = 24;
+    ((BMP_HEADER*)tr_pp)->compression = 0;
+    ((BMP_HEADER*)tr_pp)->imageSize = wsize * hsize * 3;
+    ((BMP_HEADER*)tr_pp)->XpixcelPerMeter = 0xec4;
+    ((BMP_HEADER*)tr_pp)->YpixcelPerMeter = 0xec4;
+    ((BMP_HEADER*)tr_pp)->numberOfColors = 0;
+    ((BMP_HEADER*)tr_pp)->colorsImportant = 0;
+
+    moto_pp = dst1_pp;
+    for (i = 0; i < hsize; i++) {
+        imgtr_pp = tr_pp + (hsize - i - 1) * wsize * 3 + sizeof(BMP_HEADER);
+        for (j = 0; j < wsize; j++) {
+            u_char r = moto_pp[0];
+            u_char g = moto_pp[1];
+            u_char b = moto_pp[2];
+            moto_pp += 4;
+            
+            imgtr_pp[2] = r;
+            imgtr_pp[1] = g;
+            imgtr_pp[0] = b;
+            imgtr_pp += 3;
+        }
+    }
+
+    sceLseek(fd, 0, 0);
+    sceWrite(fd, tr_pp + 2, wsize * hsize * 3 + (sizeof(BMP_HEADER) - 2));
+    free(tr_pp);
+    free(dst1_pp);
+    sceClose(fd);
+}
+#else
 INCLUDE_ASM(const s32, "dbug/vramsave", VramSaveBMP);
+#endif
 
 INCLUDE_ASM(const s32, "dbug/vramsave", VramSaveBMPDouble);
 
